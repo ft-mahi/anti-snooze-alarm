c77ea827120b7d2f9dd50dfe80b758502b2a7cdd
MAHI. <promptwithmahi@gmail.com>
2026-06-09 22:04:56 +0600

    Update alarm time and secret key values

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # config.py
 
 # Set your alarm time in 24-hour format (HH:MM)
-ALARM_TIME ="21:24"
+ALARM_TIME ="07:00"
 
 # The unique data inside the barcode you chose.
 # Pythonista's scanner reads barcodes/QR codes as strings.
-SECRET_KEY = "iam ok"
+SECRET_KEY = "Iam Ready"
